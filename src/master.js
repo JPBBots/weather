b3815d35212818554d27eaa4e12a05670288db2f
jpbberry <jb@jt3ch.net>
2021-04-01 23:09:35 -0400

    Add autoposting

--- src/master.js
+++ src/master.js
@@ -1,4 +1,6 @@
 const { Master } = require('discord-rose')
+const AutoPoster = require('topgg-autoposter')
+
 const config = require('../config')
 const path = require('path')
 
@@ -7,10 +9,13 @@ const master = new Master(path.resolve(__dirname, './worker.js'), {
   cache: {
     channels: false
   },
+  shards: 5,
   cacheControl: {
     roles: ['permissions'],
     guilds: ['owner_id']
   }
 })
+
+AutoPoster(config.dbl, master)
 
 master.start()
