6d6ac64a1fa37ec90914bb3c9ecb66cbf9396111
jpbberry <jb@jt3ch.net>
2021-05-19 22:15:21 -0400

    Added slash commands

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-const { Worker, Embed, CommandContext } = require('discord-rose')
+const { Worker, Embed, CommandContext, SlashCommandContext } = require('discord-rose')
 const { Interface } = require('interface')
 const getWeather = require('./weather')
 
@@ -16,7 +16,7 @@ const degrees = {
 
 const worker = new Worker()
 worker.db = db
-worker.interface = interface
+worker.interface = interface;
 
 interface.setupWorker(worker)
 
@@ -52,8 +52,194 @@ const weatherEmbed = (embed, response, forecast = false) => {
     .timestamp()
 }
 
+worker.commands
+  .add({
+    command: 'help',
+    aliases: ['support'],
+    interaction: {
+      name: 'help',
+      description: 'Gives help for weather bot'
+    },
+    exec: (ctx) => {
+      return ctx.embed
+        .title('Help with Weather Bot')
+        .field('How to use', `Simply do ${ctx.weatherCommand} [location], to see weather at said location\nYou can also do ${ctx.prefix} f [location], to see the forecast.`)
+        .field('More Commands', [
+          `help: Shows this menu`,
+          `settings: Customizes settings`,
+        ].map(x => `${ctx.prefix}${x}`).join('\n'))
+        .field('Helpful Links', `[Website](https://weather.jpbbots.org) | [Support Server](https://jpbbots.org/support)\n[Top.gg](https://top.gg/bot/${ctx.worker.user.id})`)
+        .send()
+    }
+  })
+  .add({
+    command: 'settings',
+    aliases: ['set', 'setloc', 'setdg'],
+    interaction: {
+      name: 'settings',
+      description: 'Changes your user settings',
+      options: [{
+        name: 'location',
+        description: 'Sets the default location',
+        type: 1,
+        options: [{
+          name: 'place',
+          required: true,
+          description: 'Default location to set',
+          type: 3
+        }]
+      }, {
+        name: 'degree',
+        description: 'Sets default degrees',
+        type: 1,
+        options: [{
+          name: 'dg',
+          description: 'Default degree to set',
+          type: 3,
+          required: true,
+          choices: [{
+            name: 'Farenheit',
+            value: 'f'
+          }, {
+            name: 'Celcius',
+            value: 'c'
+          }, {
+            name: 'Kelvin',
+            value: 'k'
+          }]
+        }]
+      }, {
+        name: 'current',
+        description: 'Gets current settings',
+        type: 1
+      }]
+    },
+    exec: async (ctx) => {
+      if (ctx.isInteraction) {
+        if (ctx.options.location) {
+          ctx.args = ['location', ctx.options.location.place]
+        } else if (ctx.options.degree) {
+          ctx.args = ['dg', ctx.options.degree.dg]
+        } else if (ctx.options.current) {
+          ctx.args = []
+        }
+      }
+      if (ctx.args[0] === 'location') {
+        if (!ctx.args[1]) return ctx.error('Missing location: Do ...set location [location]')
+        const { name } = await getWeather(ctx.args.slice(1).join(' '), 'f')
+        await db.collection('users').updateOne({ id: ctx.author.id }, {
+          $set: {
+            zip: String(ctx.args[1])
+          }
+        }, { upsert: true })
+
+        await ctx.embed
+          .color(config.color)
+          .description(`Set default location ${name}\n\nDo "${ctx.weatherCommand}" to see this location.`)
+          .send()
+      } else if (ctx.args[0] === 'dg') {
+        if (!ctx.args[1] || !Object.keys(degrees).includes(ctx.args[1])) return ctx.error(`Missing or invalid degree type, Do ...set dg [${Object.keys(degrees).join('/')}]`)
+
+        await db.collection('users').updateOne({ id: ctx.author.id }, {
+          $set: {
+            dg: ctx.args[1]
+          }
+        }, { upsert: true })
+
+        await ctx.embed
+          .color(config.color)
+          .description(`Set default degree to ${ctx.args[1]}`)
+          .send()
+      } else {
+        const db = await ctx.getDb()
+        const embed = ctx.embed
+          .color(config.color)
+          .title('Customization settings')
+          .field('Degrees', `To set your default degrees for any weather info.\n\nDo: ${ctx.prefix}set dg [${Object.keys(degrees).join('/')}]`, true)
+          .field('Default location', `To be able to just run ${ctx.weatherCommand} to see the information at that location.\n\nDo: ${ctx.prefix}set location [location]`, true)
+          .footer('Need help? Do @Weather help')
+
+        if (db) embed.description(`Current settings, degree: ${db.dg}, location: ${db.zip}`)
+
+        await embed.send()
+      }
+    }
+  })
+  .add({
+    command: 'f',
+    interaction: {
+      name: 'forecast',
+      description: 'Gets the forecast for a location',
+      options: [{
+        name: 'location',
+        description: 'Location to check the forecast in',
+        type: 3
+      }]
+    },
+    aliases: ['forecast'],
+    exec: async (ctx) => {
+      if (ctx.isInteraction) {
+        ctx.args = [ctx.options.location]
+      }
+
+      if (!ctx.args[0] && !await ctx.getZip()) return ctx.error('Missing location! Do @Weather f [location] OR run @Weather settings if you\'d like to set a default location')
+
+      const response = await getWeather(ctx.args[0] ? ctx.args.join(' ') : await ctx.getZip(), await ctx.getDegree())
+
+      const embed = weatherEmbed(ctx.embed, response, true)
+
+      const { degreeType } = response
+
+      response.forecast.forEach(day => {
+        const date = new Date(day.date.split('-').join(' ')) // properly format
+          .toDateString().split(' ') // turn into readable
+          .slice(1).slice(0, -1) // remove day name and year
+          .join(' ') // join month and day
+        embed.field(`${day.day} | ${date}`, `High: ${day.high}${deg}${degreeType}\nLow: ${day.low}${deg}${degreeType}\nPrecipitation: ${day.precip ?? '0'}%`)
+      })
+
+      await embed.send()
+    }
+  })
+  .add({
+    command: /.*/,
+    interaction: {
+      name: 'weather',
+      description: 'Gets the weather in a specific location or your default',
+      options: [{
+        name: 'location',
+        description: 'Location to get whether in',
+        type: 3
+      }]
+    },
+    exec: async (ctx) => {
+      if (ctx.isInteraction) {
+        const loc = ctx.args.shift()
+        ctx.ran = loc
+      }
+      if (!ctx.ran && !await ctx.getZip()) return ctx.error('Missing location! Do @Weather [location] OR run @Weather settings if you\'d like to set a default location')
+      const response = await getWeather(ctx.ran ? ctx.ran + ctx.args.join(' ') : await ctx.getZip(), await ctx.getDegree())
+
+      const { weather, degreeType } = response
+
+      await weatherEmbed(ctx.embed, response)
+        .thumbnail(weather.image)
+        .field('Temperature', `${weather.temperature}${deg}${degreeType}`, true)
+        .field('Feels Like', `${weather.feelsLike}${deg}${degreeType}`, true)
+        .field('Sky', weather.skyText, true)
+        .field('Humditiy', `${weather.humidity}%`, true)
+        .field('High / Low', `${weather.forecast.high}${deg}${degreeType} / ${weather.forecast.low}${deg}${degreeType}`, true)
+        .field('Precipitation', `${weather.forecast.precip ?? '0'}%`, true)
+        .send()
+    }
+  })
+
 worker.commands.CommandContext = class extends CommandContext {
   db = false
+
+  get weatherCommand () {
+    return this.prefix
+  }
 
   async getDb () {
     if (this.db !== false) return this.db
@@ -77,120 +263,32 @@ worker.commands.CommandContext = class extends CommandContext {
     return (await this.getDb())?.zip
   }
 }
+worker.commands.SlashCommandContext = class extends SlashCommandContext {
+  db = false
 
-worker.commands
-  .add({
-    command: 'help',
-    aliases: ['support'],
-    exec: (ctx) => {
-      return ctx.embed
-        .title('Help with Weather Bot')
-        .field('How to use', `Simply do ${ctx.prefix} [location], to see weather at said location\nYou can also do ${ctx.prefix} f [location], to see the forecast.`)
-        .field('More Commands', [
-          `help: Shows this menu`,
-          `settings: Customizes settings`,
-        ].map(x => `${ctx.prefix}${x}`).join('\n'))
-        .field('Helpful Links', `[Website](https://weather.jpbbots.org) | [Support Server](https://jpbbots.org/support)\n[Top.gg](https://top.gg/bot/${ctx.worker.user.id})`)
-        .send()
-    }
-  })
-  .add({
-    command: 'settings',
-    aliases: ['set', 'setloc', 'setdg'],
-    exec: async (ctx) => {
-      if (ctx.args[0] === 'location') {
-        if (!ctx.args[1]) return ctx.error('Missing location: Do ...set location [location]')
-        const { name } = await getWeather(ctx.args.slice(1).join(' '), 'f')
-        await db.collection('users').updateOne({ id: ctx.message.author.id }, {
-          $set: {
-            zip: String(ctx.args[1])
-          }
-        }, { upsert: true })
+  get weatherCommand () {
+    return '/weather'
+  }
 
-        await ctx.embed
-          .color(config.color)
-          .description(`Set default location ${name}\n\nDo "${ctx.prefix}" to see this location.`)
-          .send()
-      } else if (ctx.args[0] === 'dg') {
-        if (!ctx.args[1] || !Object.keys(degrees).includes(ctx.args[1])) return ctx.error(`Missing or invalid degree type, Do ...set dg [${Object.keys(degrees).join('/')}]`)
+  async getDb () {
+    if (this.db !== false) return this.db
 
-        await db.collection('users').updateOne({ id: ctx.message.author.id }, {
-          $set: {
-            dg: ctx.args[1]
-          }
-        }, { upsert: true })
+    this.db = await db.collection('users').findOne({ id: this.author.id })
 
-        await ctx.embed
-          .color(config.color)
-          .description(`Set default degree to ${ctx.args[1]}`)
-          .send()
-      } else {
-        const db = await ctx.getDb()
-        const embed = ctx.embed
-          .color(config.color)
-          .title('Customization settings')
-          .field('Degrees', `To set your default degrees for any weather info.\n\nDo: ${ctx.prefix}set dg [${Object.keys(degrees).join('/')}]`, true)
-          .field('Default location', `To be able to just run ${ctx.prefix} to see the information at that location.\n\nDo: ${ctx.prefix}set location [location]`, true)
-          .footer('Need help? Do @Weather help')
+    return this.db
+  }
 
-        if (db) embed.description(`Current settings, degree: ${db.dg}, location: ${db.zip}`)
+  async getDegree () {
+    if (this.flags.degree) return this.flags.degree
 
-        await embed.send()
-      }
-    }
-  })
-  .add({
-    command: 'f',
-    aliases: ['forecast'],
-    exec: async (ctx) => {
-      if (!ctx.args[0] && !await ctx.getZip()) return ctx.error('Missing location! Do @Weather f [location] OR run @Weather settings if you\'d like to set a default location')
+    const user = await this.getDb()
 
-      const response = await getWeather(ctx.args[0] ? ctx.args.join(' ') : await ctx.getZip(), await ctx.getDegree())
+    if (!user) return 'f'
 
-      const embed = weatherEmbed(ctx.embed, response, true)
+    return user.dg
+  }
 
-      const { degreeType } = response
-
-      response.forecast.forEach(day => {
-        const date = new Date(day.date.split('-').join(' ')) // properly format
-          .toDateString().split(' ') // turn into readable
-          .slice(1).slice(0, -1) // remove day name and year
-          .join(' ') // join month and day
-        embed.field(`${day.day} | ${date}`, `High: ${day.high}${deg}${degreeType}\nLow: ${day.low}${deg}${degreeType}\nPrecipitation: ${day.precip ?? '0'}%`)
-      })
-
-      await embed.send()
-    }
-  })
-  .add({
-    command: /.*/,
-    exec: async (ctx) => {
-      if (!ctx.ran && !await ctx.getZip()) return ctx.error('Missing location! Do @Weather [location] OR run @Weather settings if you\'d like to set a default location')
-      const response = await getWeather(ctx.ran ? ctx.ran + ctx.args.join(' ') : await ctx.getZip(), await ctx.getDegree())
-
-      const { weather, degreeType } = response
-
-      await weatherEmbed(ctx.embed, response)
-        .thumbnail(weather.image)
-        .field('Temperature', `${weather.temperature}${deg}${degreeType}`, true)
-        .field('Feels Like', `${weather.feelsLike}${deg}${degreeType}`, true)
-        .field('Sky', weather.skyText, true)
-        .field('Humditiy', `${weather.humidity}%`, true)
-        .field('High / Low', `${weather.forecast.high}${deg}${degreeType} / ${weather.forecast.low}${deg}${degreeType}`, true)
-        .field('Precipitation', `${weather.forecast.precip ?? '0'}%`, true)
-        .send()
-    }
-  })
-
-worker.commands.error((ctx, error) => {
-  if (!ctx.myPerms('sendMessages')) return
-  if (!ctx.myPerms('embed')) return ctx.send('Missing `Embed Link` permission.')
-  if (!ctx.myPerms('readHistory')) return ctx.send('Missing `Read Message History` permission.')
-
-  ctx.embed
-    .color(0xFF0000)
-    .title('Error occured')
-    .description(`\`\`\`\n${error.message}\`\`\``)
-    .footer('Need help? Do @Weather help')
-    .send()
-})
+  async getZip () {
+    return (await this.getDb())?.zip
+  }
+}
